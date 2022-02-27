ba4d734d94c2828d13c1b5751d7f344334d4ff87
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-02-27 13:00:06 +0100

    fix: comma

--- R/2022-03-03.R
+++ R/2022-03-03.R
@@ -2,6 +2,6 @@ create_spotlight_night(
   output = "posters/2022-03-03.png",
   rmd_params = list(
     title = "2",
-    date = "Jeudi 3 Mars 2022 à 21 h 00",
+    date = "Jeudi 3 Mars 2022 à 21 h 00"
   )
 )
