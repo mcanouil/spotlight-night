7a1e2bf1df7057df2e8daf53cd8d854c35cef855
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-02-27 11:55:03 +0100

    fix: function call

--- R/2021-09-30.R
+++ R/2021-09-30.R
@@ -1,7 +1,7 @@
 create_spotlight_night(
-  output = "posters/2022-02-18.png",
+  output = "posters/2021-09-30.png",
   rmd_params = list(
-    title = ".red[SPOT]LIGHT - Scène Ouverte .red[#1]",
+    title = "1",
     date = "Jeudi 30 Septembre 2021 à 21 h 00",
     group = "Les Sorties Lilloises (LSL)"
   )
--- R/2022-03-03.R
+++ R/2022-03-03.R
@@ -1,7 +1,7 @@
 create_spotlight_night(
-  output = "posters/2022-02-18.png",
+  output = "posters/2022-03-03.png",
   rmd_params = list(
-    title = ".red[SPOT]LIGHT - Scène Ouverte .red[#2]",
+    title = "2",
     date = "Jeudi 3 Mars 2022 à 21 h 00",
     group = "Les Sorties Lilloises (LSL)"
   )
